170935149b9a8f0113673df8a57ca48f50da07d1
czczup <wztxy89@163.com>
2020-12-13 20:50:25 +0800

    update

--- SegmenTron/test/naicrs/model_define.py
+++ SegmenTron/test/naicrs/model_define.py
@@ -11,6 +11,7 @@ from segmentron.models.backbones.ibn_shufflenetv2p import ibn_shufflenetv2_plus
 from segmentron.models.backbones.ofa_note10_lat_8ms.main import ofa_note10_lat_8ms, ibn_ofa_note10_lat_8ms
 from segmentron.models.backbones.ofa_v100_gpu64_6ms.main import ofa_v100_gpu64_6ms, ibn_ofa_v100_gpu64_6ms
 from segmentron.models.backbones.resnet import resnet50
+from segmentron.models.backbones.vovnet import vovnet19_dw
 from deeplabv3plus_nearest import DeepLabV3PlusNearest
 from tools import fuse_module
 try:
@@ -23,14 +24,14 @@ def init_model(args):
     device = torch.device("cuda")
     
     # model = DeepLabV3Plus(nclass=[8,14], get_backbone=ofa_1080ti_gpu64_27ms, channels=[32, 1664])
-    # model = DeepLabV3Plus(nclass=[8,14], get_backbone=ibn_tiny_ofa_1080ti_gpu64_27ms, channels=[32, 416])
     # model = DeepLabV3Plus(nclass=[8,14], get_backbone=shufflenetv2_plus, channels=[68,1280])
     # model = DeepLabV3Plus(nclass=[8,14], get_backbone=ibn_resnet50, channels=[256, 2048])
     # model = DeepLabV3Plus(nclass=[8,14], get_backbone=ofa_note10_lat_8ms, channels=[24, 160])
     # model = DeepLabV3Plus(nclass=[8,14], get_backbone=resnext50_32x4d, channels=[256, 2048])
-    
-    # model = DeepLabV3Plus(nclass=[8,14], get_backbone=ofa_v100_gpu64_6ms, channels=[32, 248])
-    model = DeepLabV3PlusNearest(nclass=[8,14], get_backbone=ofa_v100_gpu64_6ms, channels=[32, 248])
+    # model = DeepLabV3Plus(nclass=[8,14], get_backbone=tiny_ofa_1080ti_gpu64_27ms, channels=[32, 416])
+    model = DeepLabV3Plus(nclass=[8,14], get_backbone=ofa_v100_gpu64_6ms, channels=[32, 248])
+    # model = DeepLabV3PlusNearest(nclass=[8,14], get_backbone=ofa_v100_gpu64_6ms, channels=[32, 248])
+    # model = DeepLabV3Plus(nclass=[8,14], get_backbone=vovnet19_dw, channels=[256, 1024])
 
     model_path = "../model/%s" % args.model
     print('load test model from {}'.format(model_path))
--- SegmenTron/test/naicrs/test.py
+++ SegmenTron/test/naicrs/test.py
@@ -1,12 +1,25 @@
 from segmentron.models.backbones.ofa_v100_gpu64_6ms.main import ofa_v100_gpu64_6ms
 from deeplabv3plus import DeepLabV3Plus
 import torch
+from PIL import Image
+import numpy as np
 
+# image_mean = np.array([0.485, 0.456, 0.406])
+# image_std = np.array([0.229, 0.224, 0.225])
+# image = Image.open("0.tif")
+# image = image.resize((257, 257))
+# image = np.array(image) / 255.0
+# image = (image - image_mean) / image_std
+# image = image.transpose((2, 0, 1))
+# image = torch.Tensor(image).unsqueeze(0)
 
 model = DeepLabV3Plus(nclass=[8, 14], get_backbone=ofa_v100_gpu64_6ms, channels=[32, 248])
 state_dict = torch.load("../model/0046.pth", map_location=lambda storage, loc: storage)
 msg = model.load_state_dict(state_dict, strict=False)
 print(msg)
 model.eval()
-input = torch.ones(1, 3, 640, 640)
-out = model(input, "04")
+image = torch.ones(1, 3, 256, 256)
+out = model(image, "03")[0][0]
+print(out)
+print(out[..., 37:43, 37:46])
+print(out.shape)
